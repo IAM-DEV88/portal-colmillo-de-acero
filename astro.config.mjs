f399526341156f5d2d9e516e1d90f0e47e5d90ef
IAM-DEV88 <jaguerx88@gmail.com>
2025-10-27 23:03:09 -0500

    sistema de voto ii

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,19 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
+import netlify from '@astrojs/netlify';
+
 // https://astro.build/config
 export default defineConfig({
-  output: 'server', // Habilita el modo servidor para endpoints de API
+  output: 'server',
   integrations: [tailwind()],
+
   server: {
     port: 3000,
   },
+
+  // Reemplaza con tu URL de Netlify
+  site: 'https://tuguild.netlify.app',
+
+  adapter: netlify()
 });
