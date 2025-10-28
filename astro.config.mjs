b0979143b4c0a81d298190b43abadacba9a14063
IAM-DEV88 <jaguerx88@gmail.com>
2025-10-27 23:29:31 -0500

    Corregir configuración para Netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,17 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import netlify from '@astrojs/netlify/functions';
+import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  // @ts-ignore - Ignorar error de tipo temporalmente
-  adapter: netlify({}),
+  adapter: netlify({
+    edgeFunctions: false,
+    builders: false,
+  }),
   site: 'https://colmillo.netlify.app',
   integrations: [tailwind()],
   server: {
     port: 3000,
-  },
-  build: {
-    format: 'file',
-  },
+  }
 });
