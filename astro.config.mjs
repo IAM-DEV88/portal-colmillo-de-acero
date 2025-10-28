39ad585a8ee830c79e977f8213a1cb0e2f427e24
IAM-DEV88 <jaguerx88@gmail.com>
2025-10-27 23:09:23 -0500

    Corregir configuración de Netlify y actualizar encabezados de caché

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import netlify from '@astrojs/netlify/functions';
+import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  adapter: netlify({}),
+  adapter: netlify(),
   integrations: [tailwind()],
   server: {
     port: 3000,
